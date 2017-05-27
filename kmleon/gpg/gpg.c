585adcc4b2871bbaeb711cbf209f9604f6dd5606
stef <pitchfork@ctrlc.hu>
2017-05-27 23:22:17 +0000

    [mod] const argv in gpg

--- kmleon/gpg/gpg.c
+++ kmleon/gpg/gpg.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 // externally invoke the outdated crypto framework
-void gpg(char **argv)
+void gpg(const char **argv)
 {
 	char gpg[] = "gpg", gpg2[] = "gpg2";
 
 	argv[0] = gpg2;
-	execvp(gpg2, argv);
+	execvp(gpg2, (char**)argv);
 	argv[0] = gpg;
-	execvp(gpg, argv);
+	execvp(gpg, (char**)argv);
 
 	exit(1);
 }
